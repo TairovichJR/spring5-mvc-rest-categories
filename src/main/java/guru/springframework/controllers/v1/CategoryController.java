cdb2cd591828bd2f73c5e228bc866b7795f33190
Omonjon Yokubov <yokubov.omonjon@gmail.com>
2020-10-17 18:34:39 -0400

    Rest Controller implementation for Category controller

--- src/main/java/guru/springframework/controllers/v1/CategoryController.java
+++ src/main/java/guru/springframework/controllers/v1/CategoryController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import guru.springframework.api.v1.model.CategoryDTO;
 import guru.springframework.api.v1.model.CategoryListDTO;
@@ -15,7 +17,7 @@ import guru.springframework.services.CategoryService;
 /**
  * Created by tairovich_jr on Oct 12, 2020
  */
-@Controller
+@RestController
 @RequestMapping(CategoryController.BASE_URL)
 public class CategoryController {
 
@@ -28,15 +30,15 @@ public class CategoryController {
 	}
 
 	@GetMapping
-	public ResponseEntity<CategoryListDTO> getallCatetories() {
-
-		return new ResponseEntity<CategoryListDTO>(new CategoryListDTO(categoryService.getAllCategories()),
-				HttpStatus.OK);
+	@ResponseStatus(HttpStatus.OK)
+	public CategoryListDTO getallCatetories() {
+		return new CategoryListDTO(categoryService.getAllCategories());
 	}
 	
 	@GetMapping("/{name}")
-	public ResponseEntity<CategoryDTO> getCategoryByName(@PathVariable String name){
-		return new ResponseEntity<CategoryDTO>(categoryService.getCategoryByName(name), HttpStatus.OK);
+	@ResponseStatus(HttpStatus.OK)
+	public CategoryDTO getCategoryByName(@PathVariable String name){
+		return categoryService.getCategoryByName(name);
 	}
 	
 	
